51f0cb5fd327cbf67ec20a82c4469f13fb60f269
Rohit Vishwakarma <wishrohitv@proton.me>
2025-07-10 16:16:50 +0530

    Refactor timer countdown logic and rename functions for clarity

--- lib/utils/timer_countdown.dart
+++ lib/utils/timer_countdown.dart
@@ -1,12 +1,25 @@
 import 'dart:async';
 
-void timerCountDown(int seconds, {Function(String)? leftTime}) {
+void timerCountDown(int seconds_, {Function(String)? leftTime}) {
+  var (minute as int, second as int) = getMinuteAndSecond(seconds_);
   Timer.periodic(Duration(seconds: 1), (timer) {
-    leftTime!(secondToMinute(seconds-timer.tick));
+    if (second == 0) {
+      minute -= 1;
+      second = 60;
+    } else {
+      second -= 1;
+    }
+    leftTime?.call(formateMinuteAndsecond(minute, second));
   });
 }
 
-String secondToMinute(int seconds) {
-  double second_ = seconds / 60; // devide to convert into minute
-  return second_.toStringAsFixed(2);
+String formateMinuteAndsecond(int minute, int second) {
+  return "${minute.toString().length < 2 ? '0$minute' : minute}:${second.toString().length < 2 ? '0$second' : second}";
+}
+
+(int minute, int second) getMinuteAndSecond(int seconds_) {
+  int minute = seconds_ ~/ 60;
+  int second = seconds_ % 60;
+
+  return (minute, second);
 }
