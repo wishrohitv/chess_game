5f21baf48e6de035ee2a2c450925992d984d8c81
Rohit Vishwakarma <wishrohitv@proton.me>
2025-09-28 06:59:50 +0530

    Refactor: remove commented debug print statements from isEmpty function

--- lib/utils/is_empty.dart
+++ lib/utils/is_empty.dart
@@ -1,10 +1,7 @@
 bool isEmpty(int firstIndex, int lastIndex, int index) {
-  // print("isEmpty called");
   if (index >= firstIndex && lastIndex >= index) {
-    // print("rohit ${true}");
     return true;
   } else {
-    // print("rohit ${false}");
     return false;
   }
 }
